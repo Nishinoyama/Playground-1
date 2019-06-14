5388f63da3c9a1ac4f6f07580e380fcedf4d7146
akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2019-06-14 23:35:58 +0900

    Add files via upload

---
+++ test/test.js
@@ -0,0 +1,92 @@
+/*jshint esversion: 6 */
+//don't move
+var width = window.innerWidth;
+var height = window.innerHeight;
+var x = width/2;
+var y = height/2;
+var stage = new PIXI.Container();
+var renderer = PIXI.autoDetectRenderer(width, height,{
+    resolution: 1
+});
+document.getElementById("pixiview").appendChild(renderer.view);
+window.onresize = function () {
+    location.reload();
+};
+
+var time=0;
+function animate(){
+    requestAnimationFrame(animate);
+    time++;
+    if(time==10){
+        addobj(obj_shot*o,obj_shot*(o+1),5,"0xff000",x,y,circle);
+        for(var a=obj_shot*o;a<obj_shot*(o+1);a++){
+            for(var b=0;b<circle[a].length;b++){
+                stage.addChild(circle[a][b]);
+            }
+        }
+        o++;
+        time=0;
+    }
+
+    var l = 0;
+    var count=0;
+    for(var t=0;t<circle.length/obj_shot;t++){
+        count=0;
+        for(var i=0;i<360;i+=(360/obj_shot)){
+            if(circle[l][0]!=""){
+                var sx = circle[l][0].x;
+                var sy = circle[l][0].y;
+                xmove = sx+Math.cos(i*(Math.PI/180))*(5);
+                ymove = sy+Math.sin(i*(Math.PI/180))*(5);
+                objset(l,xmove,ymove,circle);
+                if(circle[l][0].x>x*2 || circle[l][0].x<0 || circle[l][0].y>y*2 || circle[l][0].y<0){
+                    circle[l][0].clear();
+                    circle[l][0]="";
+                    count++;
+                }
+            }else{count++;}
+            l++;
+        }
+        if(count==obj_shot){
+            circle.splice(0, obj_shot);
+            o--;
+        }
+    }
+    renderer.render(stage);
+}
+
+$(document).on("keyup",(e)=>{
+    if(e.keyCode==13){
+        addobj(obj_shot*o,obj_shot*(o+1),5,"0xff000",x,y,circle);
+        for(var a=obj_shot*o;a<obj_shot*(o+1);a++){
+            for(var b=0;b<circle[a].length;b++){
+                stage.addChild(circle[a][b]);
+            }
+        }
+        o++;
+    }
+});
+
+var circle=[];
+var obj_shot=60;
+var o=0;
+function addobj(mas,num,rad,color,x,y,obj){
+    for(var i=mas;i<num+mas;i++){
+        obj[i] = [];
+        obj[i][0] = new PIXI.Graphics();
+        obj[i][0].beginFill(color, 1);
+        obj[i][0].drawCircle(0,0,rad);
+        obj[i][0].globalCompositeOperation = 'destination-over';
+        obj[i][0].endFill();
+
+        obj[i][0].x=x;
+        obj[i][0].y=y;
+    }
+}
+
+function objset(num,x,y,obj){
+    obj[num][0].x=x;
+    obj[num][0].y=y;
+}
+
+animate();
